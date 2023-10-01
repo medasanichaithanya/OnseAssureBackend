d99704e715fec1ffc1a3e29ec8b0b10cdc411f5c
Medasani Chaithanya <61534480+medasanichaithanya@users.noreply.github.com>
2023-10-01 17:31:17 +0530

    api folder creation

--- config.py
+++
@@ -1,7 +0,0 @@
-
-
-# MONGO_URI = "mongodb+srv://chaithanya:chaithanya%401M@cluster0.v7ivcmn.mongodb.net/?retryWrites=true&w=majority"
-MONGO_URI = "mongodb+srv://chaithanya:chaithanya%401M@cluster0.v7ivcmn.mongodb.net/?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
-
-DB_NAME = "oneassure"
-COLLECTION_NAME = "insurancedata"
